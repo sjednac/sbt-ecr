a91dc3b421501d97cc4b1819779108b12f37c200
Yosef Fertel <frosforever@users.noreply.github.com>
2018-01-29 15:31:04 -0500

    Fix login error message formatting

--- src/main/scala/sbtecr/EcrPlugin.scala
+++ src/main/scala/sbtecr/EcrPlugin.scala
@@ -43,7 +43,7 @@ object EcrPlugin extends AutoPlugin {
       exec(cmd) match {
         case 0 =>
         case _ =>
-          sys.error(s"Login failed. Command: ${cmd.mkString(" ")}")
+          sys.error(s"Login failed. Command: $cmd")
       }
     },
     push := {
